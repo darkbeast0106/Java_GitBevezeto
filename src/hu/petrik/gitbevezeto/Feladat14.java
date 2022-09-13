7aeda2f0b02a2ac771d631b9af4fb6f08a9cbe12
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-13 15:45:59 +0200

    Feladat14 - Menü kiíratása, menüpont bekérése

--- src/hu/petrik/gitbevezeto/Feladat14.java
+++ src/hu/petrik/gitbevezeto/Feladat14.java
@@ -7,5 +7,17 @@ public class Feladat14 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Kérem adjon meg egy szöveget: ");
         String szoveg = sc.nextLine();
+        String menuPont = "";
+        while (!menuPont.equals("f")){
+            System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
+            System.out.println("\ta - Nagybetűssé alakítani");
+            System.out.println("\tb - Kisbetűssé alakítani");
+            System.out.println("\tc - Lekérdezni a hosszát");
+            System.out.println("\td - Összehasonlítani egy másik szöveggel");
+            System.out.println("\te - A szöveg egy részét kiiratni");
+            System.out.println("\tf - Kilépni");
+            System.out.print("Adja meg a menüpontot: ");
+            menuPont = sc.nextLine().toLowerCase();
+        }
     }
 }
